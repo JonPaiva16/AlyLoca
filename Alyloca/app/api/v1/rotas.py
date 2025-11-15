f07ad4e676f5cf47ecba4e4894c034c4af84c90f
Eduardo J Atair <eduardoatair_@hotmail.com>
2025-11-15 10:11:32 -0300

    Delete Alyloca directory

--- Alyloca/app/api/v1/rotas.py
+++
@@ -1,6 +0,0 @@
-from fastapi import APIRouter
-from app.api.v1 import produto, categoria
-
-api_rotas = APIRouter()
-api_rotas.include_router(produto.rotas)
-api_rotas.include_router(categoria.rotas)
